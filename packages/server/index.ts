0dabbeab8ca13e8ed3bcb04108ac5ac272b047a6
Mohammad Umar Ahmed <ahmedumar661@gmail.com>
2026-01-31 17:45:43 +0530

    Add and Manage Open AI API key

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import dotenv from 'dotenv';
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send("Hello World");
+})
+
+app.listen(PORT, () => {
+  console.log(`started the server on http://localhost:${PORT}`);
+})
