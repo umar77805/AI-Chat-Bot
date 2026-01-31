bfe7763d6d3d1a97f5f3bde31a2f9c0a780ae0d1
Mohammad Umar Ahmed <ahmedumar661@gmail.com>
2026-01-31 18:08:06 +0530

    Run both applications using concurrently

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently';
+
+concurrently([
+  {
+    name: 'client',
+    command: 'npm run dev',
+    cwd: 'packages/client',
+    prefixColor: "blue"
+  },
+  {
+    name: 'server',
+    command: 'npm run dev',
+    cwd: 'packages/server',
+    prefixColor: "green"
+  },
+])
